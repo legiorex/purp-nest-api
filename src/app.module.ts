e4fa14140e5633d69d1d03e6a9e27977828c969b
legiorex <batyushev@gmail.com>
2025-02-05 11:35:52 +0700

    Add MongoDB integration with Mongoose and update configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,23 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongooseConfig } from './config/mongoose.config';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule, ConfigModule.forRoot()],
+  imports: [
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+    ReviewModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: mongooseConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
